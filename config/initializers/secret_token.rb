438b1226f821a2fbfa6f7f15ec009882dbd6368f
Joshua Stickles <sticklesjb@gmail.com>
2011-12-06 21:35:08 -0500

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ElderDragonHighlander::Application.config.secret_token = 'e1d68d483afb07e6ba4f6d630e68ca27c997e6674766f5d3af48875f44d9f27772b3bd9c8def1d096f3ccb700b6cd0228eb3ff2e58e85acf0c97e366be51a96d'
